f220c1e57ea17cf542c8edf13a6bad88df1e55e3
bogyu <didqhrb1@gmail.com>
2022-07-21 21:37:53 +0900

    최단거리 자동이동 구현

---
+++ Assets/mobe.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class mobe : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    [SerializeField] Transform target;
+    NavMeshAgent agent;
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(target.position);
+        agent.SetDestination(target.position);
+    }
+}
